fbde4d0e7881a284b489d4f3de09020863c070eb
Deniz Karya Acikbas <dacikbas@umich.edu>
2026-06-12 15:30:59 -0400

    <DKA - June 12th 2026> Added LCD with OOK signal modulation ability.

---
+++ Physical Arduino/LCD + OOK Signal/Decimal_6_Binary_OOK_0110/Decimal_6_Binary_OOK_0110.ino
@@ -0,0 +1,69 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// RGB LED pins
+#define RED   9
+#define GREEN 10
+#define BLUE  11
+
+// OOK timing
+const int bitPeriodUs = 166;   // 6000 Hz bit period
+const int pulseWidthUs = 80;   // LED ON time for bit 1
+
+// LCD address and size
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  // Set RGB LED pins as outputs
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+
+  // Start LCD
+  lcd.init();
+  lcd.backlight();
+
+  // Show regular value on top
+  lcd.setCursor(0, 0);
+  lcd.print("Decimal: 6");
+
+  // Show binary value on bottom
+  lcd.setCursor(0, 1);
+  lcd.print("Binary: 0110");
+}
+
+// Turn off all RGB LED colors
+void allOff() {
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, LOW);
+}
+
+// Send bit 1 using selected LED color
+void sendOne(int pin) {
+  digitalWrite(pin, HIGH);
+  delayMicroseconds(pulseWidthUs);
+
+  digitalWrite(pin, LOW);
+  delayMicroseconds(bitPeriodUs - pulseWidthUs);
+}
+
+// Send bit 0 by keeping LED off
+void sendZero() {
+  allOff();
+  delayMicroseconds(bitPeriodUs);
+}
+
+void loop() {
+  // Start bit: 1 using RED
+  sendOne(RED);
+
+  // Message bits: 0110 using GREEN
+  sendZero();      // 0
+  sendOne(GREEN);  // 1
+  sendOne(GREEN);  // 1
+  sendZero();      // 0
+
+  // End bit: 1 using BLUE
+  sendOne(BLUE);
+}
